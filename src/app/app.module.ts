a2d7df8a4f355592d4c80b1bed09648fa06fc2a8
oletendart <letendartolivia@gmail.com>
2020-09-11 12:58:19 +0200

    delete declaration in the Module in the file app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroService } from './hero.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroesComponent 
+  ],
   bootstrap:    [ AppComponent ],
-  providers: [HeroService]
+  providers: []
 })
 export class AppModule { }
