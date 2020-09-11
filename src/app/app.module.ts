6bd6f9ae349d05fb1379867c0692f4bc63f9c394
oletendart <letendartolivia@gmail.com>
2020-09-11 12:53:15 +0200

    Create interface hero + export interface in the file heroes.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [HeroService]
 })
 export class AppModule { }
