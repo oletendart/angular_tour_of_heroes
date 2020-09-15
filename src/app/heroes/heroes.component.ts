e52bbf12d15e6099fd5e5e03979e6c1a1b40061c
oletendart <letendartolivia@gmail.com>
2020-09-15 14:21:59 +0200

    Import MessageService + insert contructor private messageService in the file heroes.component.ts

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +14,7 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
 
   constructor(
-    private heroService: HeroService
+    private heroService: HeroService, private messageService: MessageService,
     ) { }
 
   ngOnInit() {
