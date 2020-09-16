2f419ee5259b1eb734a8bff35dd5d7d67ba92137
oletendart <letendartolivia@gmail.com>
2020-09-16 23:08:37 +0200

    create const routes + insert in the import and export in the file app-routing.module.ts

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
+const routes: Routes = [ 
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
