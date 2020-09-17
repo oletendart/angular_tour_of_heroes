aeae66c4946313d4edc71206810142ca50aecfc7
oletendart <letendartolivia@gmail.com>
2020-09-17 22:38:40 +0200

    Code restructuring in the file app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroService } from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule 
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  declarations: [ 
-    AppComponent, 
-    HeroesComponent, 
-    MessagesComponent, DashboardComponent, HeroDetailComponent 
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
-  bootstrap:    [ AppComponent ],
-  providers: [HeroService, MessageService]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
