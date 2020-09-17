603731715e2f2d21962a9de6c4b4c9b9fd5a367e
oletendart <letendartolivia@gmail.com>
2020-09-17 22:57:23 +0200

    Create method private log in the file hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,9 +11,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService,
+  constructor(
+  private messageService: MessageService,
   private http: HttpClient,
-  private messageService: MessageService) { }
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
@@ -26,4 +27,8 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
 }
